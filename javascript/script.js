4e5b8b29b79c5c16fba49acef36f39cee6a7f0f6
Christopher Willians <163358050+christopherfifo@users.noreply.github.com>
2024-08-16 15:16:37 -0300

    2

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,24 @@
-const images = [
-    './imagens/1.jpg', // Substitua com o caminho para sua imagem
-    './imagens/2.jpg', // Substitua com o caminho para sua imagem
-    './imagens/3.jpg', // Substitua com o caminho para sua imagem
-    './imagens/4.jpg', // Substitua com o caminho para sua imagem
-    './imagens/5.jpg', // Substitua com o caminho para sua imagem
-    './imagens/6.jpg'
-];
 
-let currentIndex = 0;
-
-function updateImage() {
-    document.getElementById('carousel-image').src = images[currentIndex];//id de onde fica tag img
-}
-
-document.getElementById('prev-button').addEventListener('click', () => {
-    if (currentIndex === 0) {
-        currentIndex = images.length - 1;
-    } else {
-        currentIndex--;
-    }
-    updateImage();
-});
-
-document.getElementById('next-button').addEventListener('click', () => {
-    if (currentIndex === images.length - 1) {
-        currentIndex = 0;
-    } else {
-        currentIndex++;
-    }
-    updateImage();
-});
-
-// Inicializa a imagem com a primeira da lista
-updateImage();
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('.header__nav');
+    const button = document.querySelector('.header__icon-btn');
+    const navLinks = document.querySelectorAll('.nav__a');
+  
+    button.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+  
+    nav.addEventListener('click', (event) => {
+      if (event.target === nav) {
+        nav.classList.remove('active');
+      }
+    });
+  
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        nav.classList.remove('active');
+      });
+    });
+  });
+  
+  
